7a1dc975ed7157231002c60b7482a3f9202d7aa1
TTHai-Git <110751427+TTHai-Git@users.noreply.github.com>
2023-12-21 23:49:29 +0700

    Update index.py

--- app/index.py
+++ app/index.py
@@ -1,8 +1,11 @@
 from flask import render_template, request, redirect
+import dao
+from app import app, login
 from flask_login import login_user
-from app import login, dao
-from app.models import *
 
+from app.models import RoleEnum, Admin, Staff, Teacher, Student
+
+selected_role = ""
 
 @app.route('/')
 def index():
@@ -21,26 +24,34 @@ def signinandsignup():
     return render_template('signinandsignup.html')
 
 
-@app.route('/admin/login', methods=['post'])
-def admin_login():
-    request.form.get('username')
-    request.form.get('password')
-
-
 @login.user_loader
 def load_user(user_id):
-    return User.query.get(user_id)
+    if selected_role == "Admin":
+        return Admin.query.get(user_id)
+    elif selected_role == "Staff":
+        return Staff.query.get(user_id)
+    elif selected_role == "Teacher":
+        return Teacher.query.get(user_id)
+    else:
+        return Student.query.get(user_id)
 
 
-@app.route("/login-admin", methods=['get', 'post'])
-def login_admin():
+@app.route("/login", methods=['get', 'post'])
+def login():
     if request.method == 'POST':
         username = request.form.get("username")
         password = request.form.get("password")
-        user = dao.authenicate_user(username, password)
+        role = request.form.get('role')
+        global selected_role
+        selected_role = role
+        user = dao.authenicate_user(username, password, role)
         if user:
             login_user(user=user)
-    return redirect("/admin")
+            if user.role == RoleEnum.Student:
+                return redirect('/')
+            else:
+                return redirect('/admin')
+    return redirect("/signinandsignup")
 
 
 @app.route("/signupandsignin", methods=['get', 'post'])
@@ -49,4 +60,5 @@ def signupandsignin():
 
 
 if __name__ == '__main__':
+    from app import admin
     app.run(debug=True)
